084aa4d0184485484f49c9c465652555b0fad3c9
Theo Bittencourt <theodrums@gmail.com>
2013-07-25 19:01:37 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ColetivoPontual::Application.config.secret_key_base = '01cb4edf13fb7577228b3c50a5df8efe366243a1b81b640d2fe74eb1c44dbf8a8f5cf45e6e053d4f5fce9bff4a19861066b5cc33aa6afcfba45d3c2fb5c0946d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ColetivoPontual::Application.config.session_store :cookie_store, key: '_coletivo_pontual_session'
